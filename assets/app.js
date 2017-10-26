785ff4a2b8c920f7bf3c05387b097a2b303825d4
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-10-26 15:02:38 -0500

    mostly works

---
+++ assets/app.js
@@ -0,0 +1,62 @@
+
+$(document).ready(function () {
+    //Initial array
+    var shows = ["Rick and Morty", "Parks and Recreation", "Doctor Who", "Community", "30 Rock", "The Office"]
+    //Make a function to search the Giphy website
+    function displayShow() {
+        //API key
+        var giphyKey = "kXYENqTT14lojw7Hv5SK1sXRuSXP9T9y";
+        var show = $(this).attr("data-name");
+        //Search query
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=" + giphyKey + "&limit=10";
+        //Ajax call
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).done(function (response) {
+            var results = response.data;
+            console.log("Jason, you are so smart");
+            //
+            for (var i = 0; i < results.length; i += 1) {
+
+                var showDiv = $("<div class='show'>");
+
+                var p = $("<p>").text("Rating: " + results[i].rating);
+
+                var showImage = $("<img>");
+                showImage.attr("src", results[i].images.fixed_height.url);
+                showDiv.append(p);
+                showDiv.append(showImage);
+                $("#showsGoHere").prepend(showDiv);
+            }
+        })
+    };
+    //Gotta add the buttons for the TV shows. Also, we must empty the list so we aren't repeating everything.
+    function renderButtons() {
+        console.log("The buttons work so far.");
+        $("#buttons-view").empty();
+        for (var i = 0; i < shows.length; i += 1) {
+            //console.log(shows[i]);
+            var a = $("<button>");
+            a.addClass("show");
+            a.attr("data-name", shows[i]);
+            a.text(shows[i]);
+            $("#buttons-view").append(a);
+
+        };
+    };
+    //Need to add a show button from user submission
+    $("#add-show").on("click", function (event) {
+        event.preventDefault();
+        var show = $("#show-input").val().trim();
+        shows.push(show);
+        renderButtons();
+        //Need to figure out how to clear the form when user submits
+
+    })
+    //Event listener for the button click
+    $(document).on("click", ".show", displayShow);
+    //Call button function
+    renderButtons();
+
+})
